48f5eb1a4dbe46f48cc7749c85aab402c5a44ac0
Chris Danielson <christopher.a.danielson@protonmail.com>
2021-08-12 08:05:18 -0600

    Test #accept_money

---
+++ tests/cash_register_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative '../cash_register'
+require_relative '../transaction'
+
+class CashRegisterTest < Minitest::Test
+  def test_accept_money
+    register = CashRegister.new(500)
+    transaction = Transaction.new(50)
+    transaction.amount_paid = 50
+
+    previous_amount = register.total_money
+    register.accept_money(transaction)
+    current_amount = register.total_money
+
+    assert_equal previous_amount + 50, current_amount
+  end
+end
