64df7de354eb70955d358d88ae9786526df8db88
Chris Danielson <christopher.a.danielson@protonmail.com>
2021-08-12 09:40:02 -0600

    Test Text#swap

---
+++ text_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative 'text'
+
+class TextTest < Minitest::Test
+  def setup
+    @file = File.open('./sample_text.txt', 'r')
+  end
+
+  def test_swap
+    text = Text.new(@file.read)
+    vowel_swapped_text = text.swap('o', 'a')
+    expected_text = <<~TEXT.chomp
+    Larem ipsum dalar sit amet, cansectetur adipiscing elit. Cras sed vulputate ipsum.
+    Suspendisse cammada sem arcu. Danec a nisi elit. Nullam eget nisi cammada, valutpat
+    quam a, viverra mauris. Nunc viverra sed massa a candimentum. Suspendisse arnare justa
+    nulla, sit amet mallis eras sallicitudin et. Etiam maximus malestie eras, sit amet dictum
+    dalar arnare bibendum. Marbi ut massa nec larem tincidunt elementum vitae id magna. Cras
+    et varius mauris, at pharetra mi.\n
+    TEXT
+
+    p expected_text
+    p vowel_swapped_text
+    assert_equal expected_text, vowel_swapped_text
+  end
+
+  def teardown
+    @file.close
+  end
+end
